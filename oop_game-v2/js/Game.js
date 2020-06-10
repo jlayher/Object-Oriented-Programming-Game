184552b56191612818a1874716e999201f632113
jlayher <john.e.layher@gmail.com>
2020-06-10 00:47:38 -0400

    Added Classes and Methods

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+   constructor(){
+     this.missed = 0;
+     this.phrases = [];  //an array of 5 Phrase objects to use with the game. Letters and Spaces, no Numbers, Punctuation, etc.
+     this.activePhrase = null;
+   }
+
+   startGame(){
+
+   }
+
+   getRandomPhrase(){
+
+   }
+
+   handleInteraction(){
+     
+   }
+
+   removeLife(){
+
+   }
+
+   checkForWin(){
+
+   }
+
+   gameOver(){
+
+   }
+ }
