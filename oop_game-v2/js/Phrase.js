5ef56e55b1995ef339c17ca3de2e2910d441a680
jlayher <john.e.layher@gmail.com>
2020-06-13 22:45:21 -0400

    Step 7
    Created the addPhraseToDisplay method for the Phrase Class.

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -13,9 +13,38 @@
      this.phrase = phrase.toLowerCase();
    }
 
-   addPhraseToDisplay(){
+   /*
+    * Display phrase on game board
+   */
+   /*
+    * Adds letter placeholders to the display when game starts
+    * Each letter is an empty box.  One li for each letter
+    * Note any id or class attributes needed
+    * When player correctly guesses a letter, box is replaced with the letter * showMatchedLetter()
+    * Use letter CSS class for letters and space CSS class for spaces
 
-   }
+    * Split up (spread operator or split method with an empty string) each phrase into an array of characters?
+    * forEach(character)
+    * if character = letter
+      * give it the id and classes needed
+    * elseif character = space
+      * give it the id and classes needed
+   */
+   addPhraseToDisplay(){
+     const phraseUl = document.querySelector('#phrase ul');
+     const phraseCharacters = this.phrase.split('');
+     for (let i=0; i < phraseCharacters.length; i++){
+       const character = document.createElement('li');
+       if (phraseCharacters[i] === ' ') {
+         character.textContent = phraseCharacters[i];
+         character.className = 'space';
+       }else{
+         character.textContent = phraseCharacters[i];
+         character.className = `hide letter ${phraseCharacters[i]}`;
+       }
+       phraseUl.appendChild(character);
+       }
+     }
 
    checkLetter(){
 
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -10,14 +10,8 @@
 
 //test code
 
-const logPhrase = (phrase) => {
-  console.log(`Phrase - phrase: `, phrase.phrase);
-};
+
 
 const game = new Game();
 
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
+game.getRandomPhrase().addPhraseToDisplay();
