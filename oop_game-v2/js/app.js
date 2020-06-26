2d88a070ed4e3014c3e61675fa2d6cc6ca4ae533
jlayher <john.e.layher@gmail.com>
2020-06-25 23:12:24 -0400

    Maybe Done?

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -10,7 +10,9 @@
 
 // Declare Variables
 let game;
+// Selects the key button elements' div for later event delegation
 const qwerty = document.getElementById('qwerty');
+// Selects all of the key button elements
 const qwertyButtons = document.querySelectorAll('#qwerty button');
 const resetButton = document.getElementById('btn__reset');
 
@@ -33,8 +35,10 @@ qwerty.addEventListener('click', (e) => {
  * Extra Credit Part 1, Added Keyboard Functionality
  */
 window.addEventListener('keydown', (e) => {
+  //testing regex against keyboard button to make sure it is a lowercase english letter
   if (/^[a-z]+$/.test(e.key)){
     for (let i=0; i<qwertyButtons.length; i++){
+      //test if text content of quertyButton matches the value of the key pressed, and makes sure that the button is still enabled
       if (qwertyButtons[i].textContent === e.key && qwertyButtons[i].disabled === false){
         game.handleInteraction(qwertyButtons[i]);
       }
